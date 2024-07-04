f2e1fb229adcffe2d81a4a7ab93a2b82fe8fc64e
Androzelos <vejdetcubukcu@hotmail.com>
2024-07-04 15:48:55 +0300

    yaptık bir şeyler

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>@yield('title')</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -11,7 +11,8 @@
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet" href="{{asset('assets')}}/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet"
+          href="{{asset('assets')}}/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
     <link rel="stylesheet" href="{{asset('assets')}}/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
@@ -31,12 +32,12 @@
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
-@include('admin._header')
-@include('admin._sidebar')
-@section('content')
+    @include('admin._header')
+    @include('admin._sidebar')
+    @section('content')
     @show
-@include('admin._footer')
-@yield('footer')
+    @include('admin._footer')
+    @yield('footer')
 </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,13 @@ Route::get('/', function () {
 
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/home/{id}/{name}', [HomeController::class, 'test'])->where(['id' => '[0-9]+', 'name' => '[a-zA-Z]+']);
-Route::view('/', 'home.index');
+//Route::view('/', 'home.index');
 
 //Admin
 Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome');
+Route::get('/admin/login', [HomeController::class, 'login']);
+Route::post('admin/logincheck', [HomeController::class, 'logincheck'])->name('admin_logincheck');
+Route::get('admin/logout', [HomeController::class, 'logout'])->name('admin_logout');
 
 
 Route::middleware([
