436f7fa73cb1cd430f01c03d33c7063f6fb65bf6
Androzelos <vejdetcubukcu@hotmail.com>
2024-07-04 17:15:51 +0300

    yaptık bir şeyler

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,24 @@ Route::get('/', function () {
 
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/home/{id}/{name}', [HomeController::class, 'test'])->where(['id' => '[0-9]+', 'name' => '[a-zA-Z]+']);
+Route::redirect('/login','/admin/login');
 //Route::view('/', 'home.index');
 
 //Admin
-Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome');
+Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome')->middleware('auth');
 Route::get('/admin/login', [HomeController::class, 'login']);
 Route::post('admin/logincheck', [HomeController::class, 'logincheck'])->name('admin_logincheck');
 Route::get('admin/logout', [HomeController::class, 'logout'])->name('admin_logout');
+
+Route::middleware('auth')->prefix('admin')->group(function () {
+
+    Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index'])->name('admin_category');
+    Route::get('category/add', [App\Http\Controllers\Admin\CategoryController::class, 'add'])->name('admin_category_add');
+    Route::get('category/update', [App\Http\Controllers\Admin\CategoryController::class, 'update'])->name('admin_category_update');
+    Route::get('category/delete', [App\Http\Controllers\Admin\CategoryController::class, 'destroy'])->name('admin_category_delete');
+    Route::get('category/show', [App\Http\Controllers\Admin\CategoryController::class, 'show'])->name('admin_category_show');
+
+});
 
 
 Route::middleware([
